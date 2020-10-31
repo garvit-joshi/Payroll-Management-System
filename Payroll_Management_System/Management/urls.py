4a039b7208446dbc79be8a2dccc986a1913bbe67
Garvit Joshi <garvitjoshi9@gmail.com>
2020-10-31 20:36:44 +0530

    Few Tweaks

--- Payroll_Management_System/Management/urls.py
+++ Payroll_Management_System/Management/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = "Management"
 urlpatterns=[
-    path("", views.index, name="index"),
-    path("<str:name>", views.Greet, name="Greet")
+    path("", views.index, name="Index"),
+    path("Fav/", views.Fav, name="Fav"),
+    path("<str:name>", views.Greet, name="Greet"),
+    path("add/", views.add, name="Add")
 ]
--- Payroll_Management_System/Management/views.py
+++ Payroll_Management_System/Management/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+from django import forms
+
+Favorites = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    Fav = forms.CharField(label="New Favorite")
+
+
 
 # Create your views here.
 def index(request):
-    return render(request, "Management/index.html")
+    now = datetime.datetime.now()
+    return render(request, "Management/index.html", {
+        "newyear": now.month == 10 and now.day == 3
+    })
 
 def Greet(request, name):
     return render(request, "Management/greet.html", {
         "name": name.capitalize()
+    })
+
+def Fav(request):
+    return render(request, "Management/Fav.html", {
+        "Favorites": Favorites
+    })
+
+def add(request):
+    if request.method=="POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data["Fav"]
+    return render(request, "Management/Add.html", {
+        "form": NewTaskForm()
     })
