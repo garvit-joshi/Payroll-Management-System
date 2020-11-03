26f5f5348d16a34286d167d57c4f1872d3d8d4c9
Garvit Joshi <garvitjoshi9@gmail.com>
2020-11-03 14:39:18 +0530

    Changed Function,variables names and Deleted Greet function

--- Payroll_Management_System/Management/urls.py
+++ Payroll_Management_System/Management/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 app_name = "Management"
 urlpatterns=[
-    path("", views.index, name="Index"),
-    path("Fav/", views.Fav, name="Fav"),
-    path("<str:name>", views.Greet, name="Greet"),
-    path("add/", views.addFav, name="AddFav"),
-    path("addcompl/", views.addComp, name="AddComp"),
-    path("complaints/", views.View_Complaint, name="complaint")
+    path("", views.index, name="Home"),
+    path("fav/", views.view_fav, name="View_Favourite"),
+    path("addfav/", views.add_fav, name="Add_Favourite"),
+    path("addcompl/", views.add_complaint, name="Add_Complaint"),
+    path("compl/", views.view_complaint, name="View_Complaint")
 ]
--- Payroll_Management_System/Management/views.py
+++ Payroll_Management_System/Management/views.py
@@ -6,22 +6,22 @@ from django.urls import reverse
 import itertools
 
 #Adding Favorite List
-Favorites = []
+favorites = []
 
 #Adding Complaint List
-CompType = []
-CompSubject = []
-CompMessage = []
-CompAuthor = []
-CompEmail = []
+complaint_author = []
+complaint_email = []
+complaint_type = []
+complaint_subject = []
+complaint_message = []
 
 class NewFavForm(forms.Form):
-    Fav = forms.CharField(label="New Favorite", required="True")
+    new_favorite = forms.CharField(label="New Favorite", required="True")
 
 class NewComplaintForm(forms.Form):
     name = forms.CharField(label="Your name:", max_length=100, required="True")
     sender = forms.EmailField(label="Your Email:", required="True")
-    Comptype = forms.CharField(label="Complaint Type:", required="True")
+    c_type = forms.CharField(label="Complaint Type:", required="True")
     subject = forms.CharField(label="Subject:", min_length=10, required="True")
     message = forms.CharField(label="Message:",max_length=300, required="True")
 
@@ -30,52 +30,47 @@ class NewComplaintForm(forms.Form):
 def index(request):
     now = datetime.datetime.now()
     return render(request, "Management/index.html", {
-        "Favorites": Favorites
+        "Favorites": favorites
     })
 
-def Greet(request, name):
-    return render(request, "Management/greet.html", {
-        "name": name.capitalize()
-    })
-
-def Fav(request):
+def view_fav(request):
     return render(request, "Management/Fav.html", {
-        "Favorites": Favorites
+        "Favorites": favorites
     })
 
-def addFav(request):
+def add_fav(request):
     if request.method=="POST":
         form = NewFavForm(request.POST)
         if form.is_valid():
-            Fav=form.cleaned_data["Fav"]
-            Favorites.append(Fav)
-            return HttpResponseRedirect(reverse("Management:AddFav"))
+            Fav=form.cleaned_data["new_favorite"]
+            favorites.append(Fav)
+            return HttpResponseRedirect(reverse("Management:Add_Favourite"))
         else:
             return render(request, "Management/Add.html", {
                 "form": form,
-                "Favorites": Favorites
+                "Favorites": favorites
             })
     
     return render(request, "Management/Add.html", {
         "form": NewFavForm(),
-        "Favorites": Favorites
+        "Favorites": favorites
     })
 
-def addComp(request):
+def add_complaint(request):
     if request.method=="POST":
         form = NewComplaintForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data["name"]
             sender=form.cleaned_data["sender"]
-            Comptype=form.cleaned_data["Comptype"]
+            c_type=form.cleaned_data["c_type"]
             subject=form.cleaned_data["subject"]
             message=form.cleaned_data["message"]
-            CompAuthor.append(name)
-            CompType.append(Comptype)
-            CompEmail.append(sender)
-            CompMessage.append(message)
-            CompSubject.append(subject)
-            return HttpResponseRedirect(reverse("Management:AddComp"))
+            complaint_author.append(name)
+            complaint_email.append(sender)
+            complaint_type.append(c_type)
+            complaint_subject.append(subject)
+            complaint_message.append(message)
+            return HttpResponseRedirect(reverse("Management:Add_Complaint"))
         else:
             return render(request, "Management/add_complaint.html", {
                 "form": form,
@@ -85,7 +80,7 @@ def addComp(request):
         "form": NewComplaintForm()
     })
 
-def View_Complaint(request):
+def view_complaint(request):
     return render(request, "Management/complaint.html", {
-        "Complaint":zip(CompAuthor,CompEmail,CompType,CompSubject,CompMessage)
+        "Complaint":zip(complaint_author,complaint_email,complaint_type,complaint_subject,complaint_message)
     })
